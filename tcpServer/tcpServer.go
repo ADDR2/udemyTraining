fc807d77788cb89e9618efa343393a178afc39a9
Amaro Duarte <amarito93@gmail.com>
2018-01-11 17:41:29 -0500

    Adding tcp readers

--- tcpServer/tcpServer.go
+++ tcpServer/tcpServer.go
@@ -1,12 +1,17 @@
 package tcpServer
 
-import "fmt"
-import "net"
-import "log"
-import "io"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net"
+	"time"
+)
 
-/*ExecTcpServerExample is the main function of tcpServer package*/
-func ExecTcpServerExample() {
+/*ExecTCPServerExample is the main function of tcpServer package*/
+func ExecTCPServerExample() {
 	fmt.Println("|-------Beginning of tcpServer package-------|\n")
 	li, err := net.Listen("tcp", ":8080")
 	if err != nil {
@@ -28,4 +33,73 @@ func ExecTcpServerExample() {
 		conn.Close()
 	}
 	fmt.Println("\n|-------End of tcpServer pacakge-------------|\n")
+}
+
+/*ExecTCPReaderExample*/
+func ExecTCPReaderExample(ack bool) {
+	fmt.Println("|-------Beginning of tcpServer package-------|\n")
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+
+		if ack {
+			go handleWithResponse(conn)
+		} else {
+			go handle(conn)
+		}
+	}
+	fmt.Println("\n|-------End of tcpServer pacakge-------------|\n")
+}
+
+func handle(conn net.Conn) {
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+	}
+
+	defer conn.Close()
+	fmt.Println("Code got here.")
+}
+
+func handleWithResponse(conn net.Conn) {
+	err := conn.SetDeadline(time.Now().Add(10 * time.Second))
+	if err != nil {
+		log.Println("CONN TIMEOUT")
+	}
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		fmt.Fprintf(conn, "I heard you say: %s\n", ln)
+	}
+
+	defer conn.Close()
+	fmt.Println("Code got here.")
+}
+
+/*ExecTCPDialExample*/
+func ExecTCPDialExample() {
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		panic(err)
+	}
+
+	defer conn.Close()
+
+	bs, err := ioutil.ReadAll(conn)
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println(string(bs))
 }
