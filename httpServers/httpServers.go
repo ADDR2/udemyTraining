24865e0504b811159a02e4f817a4fdd8664e513d
Amaro Duarte <amarito93@gmail.com>
2018-02-01 09:30:37 -0500

    Adding views to servers

--- httpServers/httpServers.go
+++ httpServers/httpServers.go
@@ -2,8 +2,15 @@ package httpServers
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"net/url"
 )
+
+func init() {
+	tpl = template.Must(template.New("").ParseGlob("../httpServers/views/*.gohtml"))
+}
 
 type id int
 
@@ -11,10 +18,63 @@ func (identifier id) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Any code you want in this func")
 }
 
-/*ExecHttpServersExample is the main function of httpServers package*/
-func ExecHttpServersExample() {
+/*ExecHTTPServersExample is the main function of httpServers package*/
+func ExecHTTPServersExample() {
 	fmt.Println("|-------Beginning of httpServers package-------|\n")
 	var identifier id
+	http.ListenAndServe(":8080", identifier)
+	fmt.Println("\n|-------End of httpServers pacakge-------------|\n")
+}
+
+type another int
+
+var moreData bool
+
+func (identifier another) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if moreData {
+		data := struct {
+			Method        string
+			URL           *url.URL
+			Submissions   map[string][]string
+			Header        http.Header
+			Host          string
+			ContentLength int64
+		}{
+			req.Method,
+			req.URL,
+			req.Form,
+			req.Header,
+			req.Host,
+			req.ContentLength,
+		}
+
+		tpl.ExecuteTemplate(w, "form_with_details.gohtml", data)
+	} else {
+		tpl.ExecuteTemplate(w, "form.gohtml", req.Form)
+	}
+}
+
+var tpl *template.Template
+
+/*ExecHTTPFormExample  --  */
+func ExecHTTPFormExample() {
+	fmt.Println("|-------Beginning of httpServers package-------|\n")
+	var identifier another
+	moreData = false
+	http.ListenAndServe(":8080", identifier)
+	fmt.Println("\n|-------End of httpServers pacakge-------------|\n")
+}
+
+/*ExecHTTPFormDatailedExample  --  */
+func ExecHTTPFormDatailedExample() {
+	fmt.Println("|-------Beginning of httpServers package-------|\n")
+	var identifier another
+	moreData = true
 	http.ListenAndServe(":8080", identifier)
 	fmt.Println("\n|-------End of httpServers pacakge-------------|\n")
 }
